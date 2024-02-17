4c40ed0663834c8246c7dd627fd3d96698ee9090
Reed von Redwitz <reed+github@vonredwitz.com>
2024-02-17 12:58:55 +0100

    feat: start using ga4 plugin and bump versions

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { ga4Plugin } from "https://deno.land/x/fresh_ga4@0.0.4/mod.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [ga4Plugin(), tailwind()],
 });
